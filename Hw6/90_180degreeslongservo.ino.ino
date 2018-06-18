2b68017c96b9fd4db2a20f323f93dedb12612a0e
vlim15 <39513921+vlim15@users.noreply.github.com>
2018-06-17 23:38:00 -0700

    Add files via upload

---
+++ Hw6/90_180degreeslongservo.ino.ino
@@ -0,0 +1,6 @@
+ 90 x 15 = 1350 
+ //90 is the degrees and the position of the servo, times the delay milliseconds. So 90 x 15 = 1350, at 90 degrees, the servo can travel 1350 milliseconds
+
+
+ 180 x 15 = 2700
+ // 180 degrees is the position of the Servo. 180 x 15 = 2700. At 180 degree position with a 15 millisecond delay, the servo can travel 2700 milliseconds
